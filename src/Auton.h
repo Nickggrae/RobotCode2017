0494cf7dcbeb05f64c576c5fea476024cb6d51ab
liavt <liavt@yahoo.com>
2017-02-18 22:15:48 -0600

    Added another singleton to climber

--- src/Auton.h
+++ src/Auton.h
@@ -7,10 +7,11 @@ class Auton {
 	std::time_t startTime; //Used to calculate delay through time
 	int state; //Number the state machine is at
 	//singleton- hide constructor and copy constructor
-	Auton(): startTime(0), state(10) {}
+	Auton(): startTime(0), state(10) { init(); }
 	Auton(Auton const&);
 	void operator=(Auton const&);
 
+	void init();
 	//POWER ALWAYS HAS TO BE POSITIVE
 	void TurnRight(double power = 0.5);
 	void TurnLeft(double power = 0.5);
@@ -24,7 +25,6 @@ public:
 	static Auton& getInstance();
 
 	// Initialize the drivebase, set's starttime to current time, set's motor speed to half
-	void init();
 
 	//it has auton so it isnt the same as the enums it looks bad sorry
 	void TestYaw();
